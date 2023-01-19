5ec1824e81af13f91caceefacd326f5c71850ea9
VaDIM <33179761+VadimSerg@users.noreply.github.com>
2023-01-19 20:22:28 +1000

    Solved_Rest_Victory

--- restStudy/src/main/java/com/example/restStudy/config/MvcConfig.java
+++ restStudy/src/main/java/com/example/restStudy/config/MvcConfig.java
@@ -9,10 +9,8 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-//           registry.addViewController("/admin").setViewName("functions");
-     //    registry.addViewController("/admin").setViewName("listBS2");
-//
-          registry.addViewController("/admin").setViewName("index");
+
+            registry.addViewController("/admin").setViewName("index");
             registry.addViewController("/user").setViewName("user");
     }
 
