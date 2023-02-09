b010718a86b6eb47bb08d65be670b15b11daed65
VaDIM <33179761+VadimSerg@users.noreply.github.com>
2023-02-09 22:23:35 +1000

    Solved_Rest_Vict

--- restStudy/src/main/java/com/example/restStudy/controller/AdminController.java
+++ restStudy/src/main/java/com/example/restStudy/controller/AdminController.java
@@ -8,7 +8,15 @@ import com.example.restStudy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.List;
@@ -96,8 +104,5 @@ public class AdminController {
         User user = userService.getUserByName(principal.getName());
         return new ResponseEntity<>(user,HttpStatus.OK);
     }
-
-
-
 
 }
--- restStudy/src/main/java/com/example/restStudy/model/Role.java
+++ restStudy/src/main/java/com/example/restStudy/model/Role.java
@@ -2,7 +2,12 @@ package com.example.restStudy.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import java.util.Objects;
 
